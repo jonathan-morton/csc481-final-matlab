d07943aa96dc460fea4d0d774c7fda30093bc5d4
Jonathan Morton <djlarrikin@gmail.com>
2017-10-24 22:57:28 -0500

    Reads in the files and groups the same faces together

--- mortonJ_sonJ_finalProject.m
+++ mortonJ_sonJ_finalProject.m
@@ -1,1 +1,25 @@
 % Jonathan Morton and Jun Son
+imageFiles = dir('yalefaces');
+images = [];
+subjectIds = [];
+for i = 1:size(imageFiles, 1) 
+    fileName = imageFiles(i).name;
+    startSplit = 'subject';
+    endSplit = '.';
+    
+    if ~contains(fileName, startSplit, 'IgnoreCase', true)
+        continue
+    end
+    
+    subjectId = extractBetween(fileName, startSplit, endSplit);
+    subjectId = str2double(subjectId{1});
+    
+    if ismember(subjectId, subjectIds)
+         faces(subjectId).pictures{end+1} = fileName;
+    else
+        subjectIds = unique([subjectIds subjectId]);
+        faces(subjectId).id = subjectId;
+        faces(subjectId).pictures = {fileName};
+    end
+end
+    
