961824a859e087e56fb28e865db7f623296a8e21
Jun Tae Son <jtson1213@gmail.com>
2017-11-12 00:08:37 -0600

    Revert "1. script description:"

--- png2pgm_converter.m
+++
@@ -1,9 +0,0 @@
-
-% I used this script to convert your png-format images to pgm-format. 
-
-for i=1:10
-    s = sprintf('%i.png',i);
-    r = sprintf('%i.pgm',i);
-    X=imread(s);
-    imwrite(X,r)
-end
